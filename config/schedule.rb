bb8c96d2552541a7e51a342169fc12b1df28eef1
Hesham E <elnahhas@gmail.com>
2011-02-10 13:56:58 -0500

    Should not edit issue if it has been sent.

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,5 @@ every 5.minutes do
   rake "open_portch:send_issues"
 end
 
-# 1. Add the following line at the beginning of the deploy script
+# Add the following line at the beginning of the deploy script
 # require "whenever/capistrano"
-
-# 2. Append the followingto namespace :deploy
-# desc "Update the crontab file"
-# task :update_crontab, :roles => :db do
-#   run "cd #{release_path} && whenever --update-crontab #{application}"
-# end
