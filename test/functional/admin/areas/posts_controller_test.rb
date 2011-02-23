986002162eaa25a37fd9d593eb483d060a6f3a7c
Stephen McLeod <stephen@theworkinggroup.ca>
2011-02-23 13:23:07 -0500

    fixed up tests for toggle_reviewed_by

--- test/functional/admin/areas/posts_controller_test.rb
+++ test/functional/admin/areas/posts_controller_test.rb
@@ -20,10 +20,10 @@ class Admin::Areas::PostsControllerTest < ActionController::TestCase
   
   def test_update
     post = a Post
-    assert post.reviewed_by.nil?
+    assert_not_equal post.title, 'New Title'
     xhr :put, :update, :area_id => post.area, :id => post, :post => {:title => 'New Title'}
     post.reload
-    assert_equal @controller.current_user.full_name, post.reviewed_by
+    assert_equal post.title, 'New Title'
     assert_response :success
   end
   
@@ -31,6 +31,15 @@ class Admin::Areas::PostsControllerTest < ActionController::TestCase
     post = a Post
     xhr :put, :update, :area_id => post.area, :id => post, :post => {:title => ''}
     assert assigns(:post).errors[:title].include?("can't be blank")
+    assert_response :success
+  end
+  
+  def test_toggle_reviewed_by
+    post = a Post
+    assert post.reviewed_by.nil?
+    xhr :put, :toggle_reviewed_by, :area_id => post.area, :id => post
+    post.reload
+    assert_equal @controller.current_user.full_name, post.reviewed_by
     assert_response :success
   end
   
