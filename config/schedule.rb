135b37e97d3fe2416329205be55f4236d97209f8
Jack Neto <jack@theworkinggroup.ca>
2011-03-22 14:16:04 -0600

    Updated cronjob script

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :output, "log/cron.log"
 job_type :exec, "cd :path && RAILS_ENV=:environment :task :output"
 
 every 5.minutes do
-  exec "./bin/pop3.rb"
+  exec "open_porch_engine"
 end
 
 # Add the following line at the beginning of the deploy script
